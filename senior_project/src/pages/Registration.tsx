2f7a1d58c19317714db3af8abcf89e2e8c3108b5
thomkaar <thomkaar@gmail.com>
2020-05-31 01:10:54 -0700

    disables button when no terms

--- senior_project/src/pages/Registration.tsx
+++ senior_project/src/pages/Registration.tsx
@@ -1,27 +1,32 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButtons, IonBackButton, IonItem, IonButton, IonInput, IonCard, IonCardHeader, IonCardContent, IonCardTitle, IonLabel, IonModal } from '@ionic/react';
-import { Document, Page } from 'react-pdf';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButtons, IonBackButton, IonItem, IonButton, IonInput, IonCard, IonCardHeader, IonCardContent, IonCardTitle, IonLabel, IonModal, IonCheckbox } from '@ionic/react';
 import React, { useState } from 'react';
 import { arrowBack } from 'ionicons/icons';
 import { registerUser } from '../firebaseConfig';
 import { toast } from '../toast';
 import "../style/Login.css";
+import { Document, Page, pdfjs } from "react-pdf";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
 
 const Home: React.FC = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [cpassword, setCPassword] = useState('');
-  const [showModal, setShowModal] = useState(false);
-  const [numPages, setNumPages] = useState(0);
-  const [pageNumber, setpageNumber] = useState(1);
-
-  let onDocumentLoadSuccess = ({ numPages } : any) => {
-    setNumPages(numPages);
-  }
+  const [terms, setTerms] = useState(false);
   
+  let checkBox = (e: any) => {
+    setTerms(e.detail.checked);
+  };
+
+  let validPassword = () => {
+    if (password && password === cpassword)
+      return true;
+    else 
+      return false;
+  };
   async function register(){
-    setShowModal(true);
     //validation
-    if(password !== cpassword){
+    if(password !== cpassword && password !== ''){
       toast('Passwords do not match')
       return false;
     }
@@ -78,18 +83,17 @@ const Home: React.FC = () => {
               onIonChange={(e: any) => setCPassword(e.target.value)}
               />
             </IonItem>
-
-            <IonModal isOpen={showModal}>
-            <Document
-              file="consent_form_pdf.pdf"
-              onLoadSuccess={onDocumentLoadSuccess}
-            >
-              <Page pageNumber={pageNumber} />
-            </Document>
-              <IonButton onClick={() => register()}>Accpet Terms and Register</IonButton> 
-            </IonModal>
-            
-            <IonButton className="LoginButton" onClick={() => setShowModal(true)} routerLink="/registration">Register</IonButton>
+            <IonItem>
+              <IonCheckbox onIonChange={checkBox}></IonCheckbox>
+              <IonLabel>
+                <a href="https://cptc-website.s3.us-east-2.amazonaws.com/consent_form_pdf.pdf"> 
+                Accept Terms and conditions</a>
+              </IonLabel>
+            </IonItem>
+            <IonButton className="LoginButton" onClick={() => register()} 
+             disabled={!terms && !(validPassword())} 
+             routerLink="/registration">
+              Register</IonButton>
           </IonCardContent>
         </IonCard>
       </IonContent>

6d9b8465c176fe5337a76c08c67350e7ae602e2f
borajoo <chocobora1@gmail.com>
2020-07-16 20:34:55 -0700

    Added survey complete card (forgot file)

---
+++ thermalComfortApp/src/pages/SurveyComplete.tsx
@@ -0,0 +1,40 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCard, IonCardHeader,
+  IonCardSubtitle, IonCardTitle, IonCardContent, IonItem, IonIcon, IonLabel, IonButton }
+  from '@ionic/react';
+import React from 'react';
+import "../style/LandingPage.css";
+import { RouteComponentProps } from 'react-router';
+
+const SurveyComplete: React.FC<RouteComponentProps> = ({history}) => {
+  function launchHome() {
+    history.push({
+      pathname: '/landingPage',
+    });
+  }
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle></IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <IonCard>
+          <IonCardHeader>
+            <IonCardTitle>Thank you for taking our survey!</IonCardTitle>
+          </IonCardHeader>
+          <IonCardContent>
+              <IonItem lines="none">
+                <IonButton  onClick={launchHome} className="LandingButton">
+                  Return to Home
+                </IonButton>
+              </IonItem>
+            </IonCardContent>
+        </IonCard>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default SurveyComplete;
